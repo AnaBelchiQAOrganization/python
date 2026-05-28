75b91aede5d842b5e6094021718919630a660c8f
Ana Belchí <ana@anabelchi.com>
2026-05-28 18:00:27 +0200

    Update t1e1peliculas.py
    Resolvemos ejercicio 1 del tema 1: variables

---
+++ ejercicios-ana/t1e1peliculas.py
@@ -0,0 +1,55 @@
+# 1. Declaramos variables para guardar la información de mi peli favorita
+
+titulo = "Cinema paradiso"
+
+director = "Giuseppe Tornatore"
+
+anyo = 1988
+
+genero = "Drama"
+
+duracion = 124
+
+premios = True
+
+# 2. Imprimimos la info de mi peli favorita
+
+print("Mi película favorita es", titulo)
+print("La dirigió", director)
+print("Se estrenó el año", anyo)
+print("Pertenece al género", genero)
+print("Dura", duracion, "m")
+print("¿Ganó algún premio?", premios)
+print()
+
+# 2.1 - Imprimimos en tipo párrafo
+
+print("Mi película favorita es", titulo, ", dirigida por", director,
+      ". Se estrenó el año", anyo, ", perteneciendo al género", genero,
+      ", con una duración de", duracion, "minutos.")
+print("¿Ganó algún premio?", premios)
+print()
+
+# 3. Traducimos los valores de las variables a inglés
+
+titulo = "Cinema paradiso (EN)"
+
+director = "Giuseppe Tornatore (EN)"
+
+anyo = 1988
+
+genero = "Drama (EN)"
+
+duracion = 124
+
+premios = True
+
+# 4. Volvemos a imprimir la info de mi peli favorita
+
+print("Mi película favorita es", titulo)
+print("La dirigió", director)
+print("Se estrenó el año", anyo)
+print("Pertenece al género", genero)
+print("Dura", duracion, "m")
+print("¿Ganó algún premio?", premios)
+print()
