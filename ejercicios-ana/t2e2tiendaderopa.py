b5c0d15da8e8eae7ab3e66f3a88bb068610577f5
Ana Belchí <ana@anabelchi.com>
2026-05-31 10:00:48 +0200

    Update t2e2tiendaderopa.py
    Añadimos redondeo en el print de los totales

--- ejercicios-ana/t2e2tiendaderopa.py
+++ ejercicios-ana/t2e2tiendaderopa.py
@@ -18,7 +18,7 @@ precio_gorras = gorra * cantidad_gorra
 
 precio_sin_iva = precio_camisetas + precio_sudaderas + precio_gorras
 
-print("Precio sin IVA=", precio_sin_iva, "€")
+print("Precio sin IVA=", round(precio_sin_iva, 2), "€")
 
 # 4 Añadimos el 21% de IVA
 
@@ -26,4 +26,4 @@ precio_con_iva = precio_sin_iva + precio_sin_iva * 0.21
 
 # 5 Imprimimos el precio final
 
-print("Total de la compra", precio_con_iva, "€")
+print("Total de la compra", round(precio_con_iva, 2), "€")
