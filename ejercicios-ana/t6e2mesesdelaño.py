78704319694eaa8a63d501d55fdae0b089fb5aca
Ana Belchí <ana@anabelchi.com>
2026-06-06 10:41:08 +0200

    Update t6e2mesesdelaño.py

--- ejercicios-ana/t6e2mesesdelaño.py
+++ ejercicios-ana/t6e2mesesdelaño.py
@@ -11,6 +11,7 @@ numero = int(input("Escribe un número del 1 al 12: "))
 
 print("El planeta que corresponde a ese mes del año es",
       meses_del_anyo[numero-1])
+
 # 4 Si el mes es junio, muestra además el mensaje: EL MEJOR MES.
 
 if numero == 6:
