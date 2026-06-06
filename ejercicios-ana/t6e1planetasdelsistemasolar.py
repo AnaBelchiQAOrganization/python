14bdfc05f8da4199251091c18830b3d2eb8db166
Ana Belchí <ana@anabelchi.com>
2026-06-06 09:36:25 +0200

    Update t6e1planetasdelsistemasolar.py

---
+++ ejercicios-ana/t6e1planetasdelsistemasolar.py
@@ -0,0 +1,19 @@
+# 1 Creamos una lista con los 8 planetas del sistema solar
+
+planetas_sistema_solar = ["Mercurio", "Venus", "La Tierra",
+                          "Marte", "Júpiter", "Saturno", "Urano", "Neptuno"]
+
+# 2 Pedimos a la usuaria un número: el enunciado es erróneo.
+# Vamos a pedir un número para poder dar un resultado malo,
+# sin especificar el rango en el que está.
+
+numero = int(input("Escribe un número: "))
+
+# 3 Mostramos el planeta correspondiente. La variable numero será el indice de la lista.
+# Ojo: el indice empieza en 0 no en 1
+# Si el número es inválido mostraremos un mensaje de error
+
+if numero in range(1, 8):
+    print("El planeta que has elegido es", planetas_sistema_solar[numero-1])
+else:
+    print("Error: No es un número válido")
