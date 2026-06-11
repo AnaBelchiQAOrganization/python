dd091b888bcba025ef01de908b01112c7e920ff3
Ana Belchí <ana@anabelchi.com>
2026-06-11 16:43:04 +0200

    tres ejercicios de cadenas

---
+++ ejercicios-ana/zejercicio21.py
@@ -0,0 +1,14 @@
+# Escribir un programa que pregunte el nombre completo del usuario en la consola y
+# después muestre por pantalla el nombre completo del usuario tres veces,
+# una con todas las letras minúsculas, otra con todas las letras mayúsculas y
+# otra solo con la primera letra del nombre y de los apellidos en mayúscula.
+# El usuario puede introducir su nombre combinando mayúsculas y minúsculas como quiera.
+
+nombre = input("Escribe tu nombre completo: ")
+
+print()
+print("Tu nombre en minúsculas es " + nombre.lower())
+print()
+print("Tu nombre en mayúsculas es " + nombre.upper())
+print()
+print("Tu nombre en letras capitales es " + nombre.title())
