aa93d3a8f5947214e85c813814c3b0494300836e
Ana Belchí <ana@anabelchi.com>
2026-06-15 21:18:28 +0200

    cambios ejercicio 5

--- ejercicios-ana/t5e2premiocolores.py
+++ ejercicios-ana/t5e2premiocolores.py
@@ -1,7 +1,7 @@
 # 1 Pedimos a la usuaria 5 veces que introduzca un color.
 
 for i in range(5):
-    color = input("Escribe un color: ")
+    color = input("Escribe un color: ").lower()
     if color == "azul":
         print()
         print("¡Enhorabuena! Has sido premiada.")
